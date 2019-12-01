84e0bfc5718ba4b1d578e15dae11facc799012ac
w3liu <w3liu@qq.com>
2019-12-01 17:41:37 +0800

    新增 算法题

--- algorithm/019/main.go
+++ algorithm/019/main.go
@@ -38,33 +38,45 @@ type ListNode struct {
 }
 
 func insertionSortList(head *ListNode) *ListNode {
-	curNode := head.Next
-	headNode := head
-	tailNode := head
-
-	for curNode != nil {
-		tempNode := curNode.Next
-
-		if headNode.Val > curNode.Val {
-			curNode.Next = headNode
-			headNode = curNode
-		} else if tailNode.Val <= curNode.Val {
-			tailNode.Next = curNode
-			tailNode = curNode
-		} else {
-			seqNode := headNode
-			for seqNode != tailNode {
-				if seqNode.Next.Val > curNode.Val {
-					curNode.Next = seqNode.Next
-					seqNode.Next = curNode
-					break
-				} else {
-					seqNode = seqNode.Next
-				}
-			}
-		}
-		curNode = tempNode
+	if head == nil {
+		return head
 	}
-	tailNode.Next = nil
-	return headNode
+	origin := &ListNode{Val: 0, Next: head}
+	curr := head
+	tail := head
+	for curr != nil {
+		next := curr.Next
+		// 当前节点和尾部节点相遇，不做任何处理，直接进入下一轮
+		if tail == curr {
+			curr = next
+			continue
+		}
+		// 若干当前节点值大于尾部节点，尾部节点与当前节点做交换
+		if curr.Val >= tail.Val {
+			tail.Next = curr
+			tail = curr
+		} else {
+			// 取出head节点的第一个节点
+			seq := origin.Next
+			// 取出origin节点的第一个节点（head的前置节点）
+			pre := origin
+			// 从头到尾开始遍历节点
+			for seq != tail {
+				// 如果遇到有大于当前节点的，则中断
+				if seq.Val > curr.Val {
+					break
+				}
+				pre = seq
+				seq = seq.Next
+			}
+			// 执行插入排序
+			curr.Next = seq
+			pre.Next = curr
+		}
+
+		curr = next
+	}
+	// 尾部的Next需要置空，要不可能会出现死循环
+	tail.Next = nil
+	return origin.Next
 }
