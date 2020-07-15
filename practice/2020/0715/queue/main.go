4ac2c4f00eff3be150db9ea17b781c515b424854
w3liu <w3liu@qq.com>
2020-07-15 22:22:06 +0800

    新增 practice #queue#

---
+++ practice/2020/0715/queue/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+	q := NewArrayQueue(10)
+	for i := 0; i < 10; i++ {
+		q.enQueue(fmt.Sprintf("%d", i))
+		fmt.Println("enQueue ", i)
+	}
+	for true {
+		ret := q.deQueue()
+		if ret == "" {
+			break
+		}
+		fmt.Println("deQueue ", ret)
+	}
+}
+
+type ArrayQueue struct {
+	items []string
+	n     int
+	head  int
+	tail  int
+}
+
+func NewArrayQueue(n int) *ArrayQueue {
+	return &ArrayQueue{
+		items: make([]string, n),
+		n:     n,
+		head:  0,
+		tail:  0,
+	}
+}
+
+// 入队
+func (q *ArrayQueue) enQueue(item string) bool {
+	if q.tail == q.n {
+		return false
+	}
+	q.items[q.tail] = item
+	q.tail++
+	return true
+}
+
+// 出队
+func (q *ArrayQueue) deQueue() string {
+	if q.head == q.tail {
+		return ""
+	}
+	ret := q.items[q.head]
+	q.head++
+	return ret
+}
