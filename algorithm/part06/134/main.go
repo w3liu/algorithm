d587e592fcc70983bd5f751be4a33ead33930ce3
w3liu <w3liu@qq.com>
2022-05-29 22:42:18 +0800

    docs: add algorithm

--- algorithm/part06/134/main.go
+++ algorithm/part06/134/main.go
@@ -4,6 +4,27 @@ func main() {
 
 }
 
-func findAnagrams(s string, p string) []int {
+func findAnagrams(s, p string) (ans []int) {
+	sLen, pLen := len(s), len(p)
+	if sLen < pLen {
+		return
+	}
 
+	var sCount, pCount [26]int
+	for i, ch := range p {
+		sCount[s[i]-'a']++
+		pCount[ch-'a']++
+	}
+	if sCount == pCount {
+		ans = append(ans, 0)
+	}
+
+	for i, ch := range s[:sLen-pLen] {
+		sCount[ch-'a']--
+		sCount[s[i+pLen]-'a']++
+		if sCount == pCount {
+			ans = append(ans, i+1)
+		}
+	}
+	return
 }
